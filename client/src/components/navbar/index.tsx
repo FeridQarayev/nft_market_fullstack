8ad94e2bae9688f6016c4e682f124703b637f173
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-20 19:51:00 +0400

    navbar style add tablet

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo_store from "../../images/navbar/storefront-2@2x.svg";
 import logo_text from "../../images/navbar/nft-marketplace-1@2x.svg";
 import logo_user from "../../images/navbar/user@2x.svg";
+import logo_burger from "../../images/navbar/burger-menu-@2x.svg";
 import { Link } from "react-router-dom";
 import style from "./navbar.module.scss";
 
@@ -36,6 +37,9 @@ function Navbar() {
           <img src={logo_user} alt="User" />
           <div>Sign Up</div>
         </Link>
+      </div>
+      <div className={style.navbar__burger}>
+        <img src={logo_burger} alt="Burger Menu " />
       </div>
     </div>
   );
