b2a5f5effd2028ceff50bd2f36e431917452f396
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-20 13:59:05 +0400

    add navbar scss

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -3,35 +3,36 @@ import logo_store from "../../images/navbar/storefront-2@2x.svg";
 import logo_text from "../../images/navbar/nft-marketplace-1@2x.svg";
 import logo_user from "../../images/navbar/user@2x.svg";
 import { Link } from "react-router-dom";
+import style from "./navbar.module.scss";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <div className="navbar__left">
-        <div className="navbar__left__logo">
+    <div className={style.navbar}>
+      <div className={style.navbar__left}>
+        <div className={style.navbar__left__logo}>
           <img
-            className="navbar__left__logo__store"
+            className={style.navbar__left__logo__store}
             src={logo_store}
             alt="Storefront"
           />
           <img
-            className="navbar__left__logo__text"
+            className={style.navbar__left__logo__text}
             src={logo_text}
             alt="NFT Marketplace"
           />
         </div>
       </div>
-      <div className="navbar__right">
-        <Link to="">
+      <div className={style.navbar__right}>
+        <Link className={style.navbar__right__link} to="">
           <div>Marketplace</div>
         </Link>
-        <Link to="">
+        <Link className={style.navbar__right__link} to="">
           <div>Rankings</div>
         </Link>
-        <Link to="">
+        <Link className={style.navbar__right__link} to="">
           <div>Connect a wallet</div>
         </Link>
-        <Link to="">
+        <Link className={style.navbar__right__btn} to="">
           <img src={logo_user} alt="User" />
           <div>Sign Up</div>
         </Link>
