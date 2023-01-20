95e14130f6d238d549027b143b45fa5ee4cca6b8
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-20 14:17:15 +0400

    headline in home

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import style from "./home.module.scss";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={style.headline}>
+      <div className={style.headline__container}>
+        <div className={style.headline__container__top}>
+          <div className={style.headline__container__top__text}>
+            Top Creators
+          </div>
+        </div>
+        <div className={style.headline__container__down}>
+          <p>Check out top ranking NFT artists on the NFT Marketplace.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
