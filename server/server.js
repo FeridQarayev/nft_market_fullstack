cf0fa342a6c35032fd625fc439765a8eef3cf5af
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-21 20:04:42 +0400

    connect to mongo db

--- server/server.js
+++ server/server.js
@@ -6,31 +6,40 @@ const app = express();
 
 const PORT = 8080;
 
-const NFTSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  highest: Number,
-  artistName: String,
-  artistImage: String,
-});
+// const NFTSchema = new mongoose.Schema({
+//   name: String,
+//   price: Number,
+//   highest: Number,
+//   artistName: String,
+//   artistImage: String,
+// });
 
-const ArtistSchema = new mongoose.Schema({
-  name: String,
-  change: Number,
-  sold: Number,
-  volume: Number,
-  imgUrl: String,
-  nfts:[{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-  }]
-});
+// const ArtistSchema = new mongoose.Schema({
+//   name: String,
+//   change: Number,
+//   sold: Number,
+//   volume: Number,
+//   imgUrl: String,
+//   nfts:[{
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "Product",
+//   }]
+// });
 
-const NFTModel = mongoose.model("NFT", NFTSchema);
-const ArtistModel = mongoose.model("Artist", ArtistSchema);
+// const NFTModel = mongoose.model("NFT", NFTSchema);
+// const ArtistModel = mongoose.model("Artist", ArtistSchema);
 
+mongoose.set("strictQuery", false);
 
+mongoose
+  .connect(
+    `mongodb+srv://ferid:PSZde9zf0vkXCbWc@cluster0.tnvvtt5.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.error(err));
 
 app.listen(PORT, () => {
   console.log("Server running on " + PORT);
 });
+
+// PSZde9zf0vkXCbWc
