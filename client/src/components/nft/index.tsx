ebe97a35f81b49be8e3d524b2f2339a033ef0a0b
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-21 15:57:29 +0400

    get all nft to array

--- client/src/components/nft/index.tsx
+++ client/src/components/nft/index.tsx
@@ -1,21 +1,37 @@
-import React from "react";
-import nft_img from "../../images/nft/image-placeholder@2x.png";
-import nft_img_avatar from "../../images/artist/avatar-placeholder-5@2x.png";
 import "./nft.scss";
+interface INFT {
+  id: number;
+  name: string;
+  price: number;
+  highest: number;
+  img: string;
+  artist: {
+    artistId: number;
+    name: string;
+    img: string;
+  };
+}
 
-function NFT() {
+function NFT(data: { nft: INFT }) {
+  const { nft } = data;
   return (
     <div className="nft">
       <div className="nft__img">
-        <img src={nft_img} alt="Image Placeholder" />
+        <img
+          src={require(`../../images/nft/${nft.img}`)}
+          alt="Image Placeholder"
+        />
       </div>
       <div className="nft__body">
         <div className="nft__body__artist">
-          <div className="nft__body__artist__name">Magic Mushroom 0325</div>
+          <div className="nft__body__artist__name">{nft.name}</div>
           <div className="nft__body__artist__avatar">
             <div className="nft__body__artist__avatar__img">
               <div>
-                <img src={nft_img_avatar} alt="Avatar Placeholder" />
+                <img
+                  src={require(`../../images/artist/${nft.artist.img}`)}
+                  alt="Avatar Placeholder"
+                />
               </div>
             </div>
             <div className="nft__body__artist__avatar__name">Shroomie</div>
@@ -24,11 +40,13 @@ function NFT() {
         <div className="nft__body__info">
           <div className="nft__body__info__price">
             <div className="nft__body__info__price__text">Price</div>
-            <div className="nft__body__info__price__price">1.63 ETH</div>
+            <div className="nft__body__info__price__price">{nft.price} ETH</div>
           </div>
           <div className="nft__body__info__highest">
             <div className="nft__body__info__highest__text">Highest Bid</div>
-            <div className="nft__body__info__highest__price">0.33 wETH</div>
+            <div className="nft__body__info__highest__price">
+              {nft.highest} wETH
+            </div>
           </div>
         </div>
       </div>
